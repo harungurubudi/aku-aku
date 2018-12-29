4997ba63cfe880c05143c09007ecffcc855fde2e
Damaera <damaera@live.com>
2018-12-29 12:29:50 +0700

    add routes

--- routes.js
+++ routes.js
@@ -1,4 +1,6 @@
 const nextRoutes = require("next-routes");
 const routes = (module.exports = nextRoutes());
 
+routes.add("index", "/");
 routes.add("about", "/about/:slug");
+routes.add("editor", "/editor");
